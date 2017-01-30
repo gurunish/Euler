b910fa71c0dca77581ba7204a33ce873f44319d0
gurunish <i.dunt.know@mail.com>
2017-01-30 15:11:41 +0000

    17 complete

--- src/EulerSeventeen.java
+++ src/EulerSeventeen.java
@@ -3,34 +3,49 @@
  */
 public class EulerSeventeen {
     public static void main(String[] args){
-        String[] ones={"zero","one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        String[] less={"","one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+                "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
         String[] tens={"ten","twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
         String hund ="hundred";
         String and = "and";
         String thou = "thousand";
 
-        String[] temp = new String[1000];
         int wordSum = 0;
-        for(int i=1; i<1001;i++){
-            temp[i-1] = ones[i];
+        for(int i=1; i<1000;i++){
             String tempWord = "";
-
-            // TODO - 11-19 NOT DONE
             if(i<100){
-                if(i%10==0){
-                    i /= 10;
-                    tempWord += ones[i];
+                if(i<20) {
+                    //1-19
+                    tempWord += less[i];
                 } else {
-                    tempWord += tens[i/10 -1] + ones[i];
+                    //20-99
+                    tempWord += tens[(i/10)-1] + less[i%10];
                 }
             } else if(i%100==0){
-                tempWord += ones[i/100] + hund;
+                //100,200,...,900
+                tempWord += less[i/100] + hund;
             } else {
-                tempWord += ones[i/100] + and + tens[i/10-1] + "-"+ ones[i];
+                int tensAndOnes = (i-(i/100*100));
+                if(tensAndOnes<20) {
+                    //101-119, 201-219,...,901-919
+                    if(tensAndOnes==10){
+                        tempWord += less[i/100] + hund + less[tensAndOnes];
+                    } else {
+                        tempWord += less[i / 100] + hund +"-"+ less[tensAndOnes];
+                    }
+                } else if(tensAndOnes%10==0){
+                    //110,120,...,990
+                    tempWord += less[i/100] + hund + tens[ tensAndOnes/10 -1];
+                } else {
+                    //All else
+                    System.out.println(i);
+                    tempWord += less[i/100]+ hund + and + tens[tensAndOnes/10-1] + "-"+less[i%10];
+                }
             }
-            wordSum+= temp[i-1].length();
+            wordSum+= tempWord.length();
         }
-
+        String s = "onethousand";
+        wordSum += s.length();
         System.out.println(wordSum);
     }
 }
